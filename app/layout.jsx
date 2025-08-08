b8f27dbfe1bd4cbb2c953fe4b07eaac84f6f6e4c
gautamrn <gautamrn@umich.edu>
2025-08-07 21:31:26 -0400

    layout boilerplate

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Tag.ai',
+  description: 'Train custom models to categoriza your music'
+}
+
+export default function RootLayou({children}){
+  return(
+    <html lang='en'>
+      <head />
+      <body>
+        <header style={{padding: '1rem', background: '#eee'}}>
+          <h1>Tag.ai</h1>
+        </header>
+        <main>{children}</main>
+        <footer style={{padding: '1rem', background: '#eee'}}>
+          <small>© Gautam Nigam</small>
+        </footer>
+      </body>
+    </html>
+  );
+}
