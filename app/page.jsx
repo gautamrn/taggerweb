7af4613979ae58f983d501e452c3ea49216d16b2
gautamrn <gautamrn@umich.edu>
2025-08-07 21:55:18 -0400

    more starter layouts

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function HomePage(){
+  return(
+    <section style={{padding: '2rem'}}>
+      <h2>Welcome to Tag.ai!</h2>
+      <p>Are you a dj looking to simplify tagging hundreds of songs? Let ai do it!</p>
+      <a href="/tags">Get started</a>
+    </section>
+  );
+}
---
+++ app/tags/page.jsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useState } from "react"
+
+export default function TagsPage(){
+    const [tagName, setTageName] = useState('');
+    const [files, setFiles] = useState([]);
+    const [msg, setMsg] = useState('');
+
+    const handleTagChange = e => setTageName(e.target.value);
+
+    const handleFileChange = e => setFiles(Array.from(e.target.files));
+
+    const handleUpload = async () => {
+        if(!tagName || files.length < 10){
+            setMsg('Create a tag and upload atleast 10 songs');
+            return;
+        }
+        const formData = new formData();
+        formData.append('tag', tagName);
+        files.forEach(f => formData.append('tracks', f));
+
+        const res = await fetch('/api/upload', {method: 'Post', body: formData});
+        setMsg(res.ok ? 'Upload success. Training will start' : 'Upload failed');
+        
+    }
+
+    return(
+        <main style={{padding: '2rem'}}>
+            <h2>Create a tag and upload songs that fit the tag</h2>
+            <label>
+                Tag name: <input value={tagName} onChange={handleTagChange} />
+            </label>
+
+            <div style={{margin: '1rem 0'}}>
+                <label>
+                    Audio files <input type="file" multiple accept=".mp3, .wav" onChange={handleFileChange}/>
+                </label>
+                {files.length > 0 && <p>{files.length} files(s) selected</p>}
+            </div>
+
+            <button onClick={handleUpload}>Upload and Train</button>
+
+            {msg && <p style={{ marginTop: '1rem' }}>{msg}</p>}
+        </main>
+    );
+}
